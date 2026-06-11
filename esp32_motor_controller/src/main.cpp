dfdb1406ef0179107e9462f24b512700c15e049d
kenkohoshi <hitsujikun30@gmail.com>
2026-06-11 19:44:27 +0900

    pwmでモータを鳴らします

--- esp32_motor_controller/src/main.cpp
+++ esp32_motor_controller/src/main.cpp
@@ -1,63 +1,24 @@
-#include<Arduino.h>
+#include <Arduino.h>
 
-#define PIN1 26
-#define PIN2 25
-#define PIN3 33
-#define PIN4 32
-#define C4 262
-int stepIndex = 0;
+const int pin = 25;
 
-void stepMotor(){
+void setup() {
+  Serial.begin(115200);
 
-  digitalWrite(PIN1, LOW);
-  digitalWrite(PIN2, LOW);
-  digitalWrite(PIN3, LOW);
-  digitalWrite(PIN4, LOW);
+  ledcSetup(0, 1000, 8);      // チャンネル0, 1kHz, 8bit
+  ledcAttachPin(pin, 0);      // ピンとチャンネルを接続
 
-  switch(stepIndex){
-    case 0: digitalWrite(PIN1, HIGH); break;
-    case 1: digitalWrite(PIN2, HIGH); break;
-    case 2: digitalWrite(PIN3, HIGH); break;
-    case 3: digitalWrite(PIN4, HIGH); break;
-  }
-
-  stepIndex++;
-
-  if(stepIndex >= 4){
-    stepIndex = 0;
-  }
-
+  ledcWrite(0, 0);
 }
 
-// 音を鳴らす
-// ----------------------
-void playNote(int freq, int duration){
+void loop() {
+  if (Serial.available()) {
+    int freq = Serial.parseInt();
 
-  if(freq == 0){
-    delay(duration);
-    return;
+    if (freq > 0) {
+      ledcSetup(0, freq, 8);
+      ledcAttachPin(pin, 0);
+      ledcWrite(0, 128);
+    }
   }
-
-  int delay_us = 1000000 / freq / 4;
-
-  unsigned long start = millis();
-
-  while(millis() - start < duration){
-
-    stepMotor();
-
-    delayMicroseconds(delay_us);
-  }
-}
-
-void setup(){
-
-  pinMode(PIN1, OUTPUT);
-  pinMode(PIN2, OUTPUT);
-  pinMode(PIN3, OUTPUT);
-  pinMode(PIN4, OUTPUT);
-}
-
-void loop(){
-	playNote(C4, 500);
 }
